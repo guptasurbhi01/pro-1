0230f6ef253eaf70add152bc5a489def5ee2453f
Surbhi gupta <46301897+guptasurbhi01@users.noreply.github.com>
2024-01-12 18:53:25 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+let test = document.getElementById("mydiv");
+test.style.backgroundColor = "yellow";
+test.style.color = "blue";
+test. style.margin = "20px";
+test.style.padding = "20px";
+test.style.fontSize = "18px";
+test.style.fontWeight = "bold";
+test.style.height = "200px";
+test.style.width = "300px";
